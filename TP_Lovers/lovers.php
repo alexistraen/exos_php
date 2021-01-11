2021c9e3ec26aae86961806dfd13b1124ec677b7
alexistraen <alexis.traen@gmail.com>
2021-01-11 17:00:21 +0100

    last commit

--- TP_Lovers/lovers.php
+++ TP_Lovers/lovers.php
@@ -129,7 +129,7 @@ $user_datas = [
 <html lang="fr">
 
 <head>
-    <title>Title</title>
+    <title>Dual Love</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -141,11 +141,39 @@ $user_datas = [
 
 <body>
 
+
+    <div id='nav-container'>
+
+        <div class="container-fluid col-12 p-0 m-0">
+            <div id="home" class="bg-dark text-center py-2">
+                <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li>
+                             <a class="navbar-brand text-uppercase"><?= 'Bonjour ' . $_COOKIE['user_firstName'] . ' <i class="fas fa-heart text-danger"></i>' ?></a>
+                            </li>
+                            <li>
+                                <a class="navbar-brand text-uppercase text-white mx-5" href="user.php">Nos célibataires</a>
+                            </li>
+                            <li>
+                                <a class="navbar-brand text-uppercase text-white mx-5" href="index.php">Inscription</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/d89aa1e4ce.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
